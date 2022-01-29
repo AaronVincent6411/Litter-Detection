c3b2ed222b85cab6c3de519d6e7f22f695611d9b
Aaron Vincent 6411 <aaronvincent6411@gmail.com>
2022-01-29 08:13:32 -0500

    Add files via upload

---
+++ Litter_Detection.py
@@ -0,0 +1,31 @@
+import cv2
+
+path = 'haarcascades/haarcascade_frontalface_default.xml'
+color= (255,0,255)
+
+cap = cv2.VideoCapture(0)
+cap.set(3, 640)
+cap.set(4, 480)
+
+
+def empty(a):
+    pass
+cascade = cv2.CascadeClassifier(path)
+
+while True:
+    success, img = cap.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    scaleVal =1 + (cv2.getTrackbarPos("Scale", "Result") /1000)
+    neig=cv2.getTrackbarPos("Neig", "Result")
+    objects = cascade.detectMultiScale(gray,scaleVal, neig)
+    for (x,y,w,h) in objects:
+        area = w*h
+        minArea = cv2.getTrackbarPos("Min Area", "Result")
+        if area >minArea:
+            cv2.rectangle(img,(x,y),(x+w,y+h),color,3)
+            cv2.putText(img,'Litter',(x,y-5),cv2.FONT_HERSHEY_COMPLEX_SMALL,1,color,2)
+            roi_color = img[y:y+h, x:x+w]
+
+    if cv2.waitKey(1) & 0xFF == ord('s'):
+        cv2.imshow("Result", img)
+        cv2.waitKey(1)
